de86be65342065e09f1afaa5b6f6037341b7acb2
Sam Maurer <maurer@berkeley.edu>
2017-09-17 16:44:03 -0700

    First pass, untested

--- download_automator/download_automator.py
+++ download_automator/download_automator.py
@@ -1,5 +1,5 @@
 __author__ = "Sam Maurer"
-__date__ = "October 14, 2016"
+__date__ = "September 17, 2017"
 __license__ = "MIT"
 
 
@@ -14,27 +14,30 @@ from keys import *   # keys.py in same directory
 
 
 OUTPUT_PATH = 'data/'  # output path relative to the script calling this class
-FNAME_BASE = 'stream-'  # default filename prefix (timestamp will be appended)
-TIME_LIMIT = 0  # default time limit in seconds, 0 for none
+FNAME_BASE = 'retrospective-'  # default filename prefix (timestamp will be appended)
 ROWS_PER_FILE = 500000  # 500k tweets is about 1.6 GB uncompressed
 DELAY = 5.0  # initial reconnection delay in seconds
-BBOX = '-126,29,-113,51'  # default bounding box (US west coast)
+RATE_LIMIT = 1.0  # pause between queries, in seconds
 COMPRESS = True  # whether to zip the resulting data file
 
 
-class Stream(object):
+class Downloader(object):
 
     def __init__(
             self,
-            fname_base = FNAME_BASE,
-            time_limit = TIME_LIMIT,
-            bbox = BBOX ):
+            user_ids,
+            ts_min,
+            ts_max,
+            geo_only = False,
+            fname_base = FNAME_BASE ):
     
         self.api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
 
+        self.user_ids = user_ids
+        self.ts_min = ts_min
+        self.ts_max = ts_max
+        self.geo_only = geo_only
         self.fname_base = fname_base
-        self.time_limit = time_limit
-        self.bbox = bbox
 
         self.t0 = None  # initialization time
         self.fpath = ''  # output filepath
@@ -48,22 +51,33 @@ class Stream(object):
         return
     
     
-    def begin_stream(self):
-        '''Initialize the streaming connection and reconnect if needed'''
+    def page_tweets(self, user_id, last_post_id=None):
+        '''Request the next batch of tweets'''
+        
+        endpoint = 'statuses/user_timeline'
+        if not last_post_id:
+            # First request for this user
+            params = {'user_id': user_id, 
+                'count': 200, 
+                'include_rts': 'false' }
+        else:
+            # Subsequent requests
+            params = {'user_id': user_id, 
+                'count': 200, 
+                'max_id': last_post_id - 1, 
+                'include_rts': 'false' }
+        
+        return api.request(endpoint, params)
+
+    
+    def manage_download(self):
+        '''Initialize and manage the download process'''
         
         while True:
-            try:
-                r = self.api.request('statuses/filter', 
-                        {'locations': self.bbox, 'stall_warnings': 'true'})
-                _test = r.get_iterator()
-                print "\n" + str(dt.now())
-                print "Connected to streaming endpoint"
-                self.t0 = time.time()
-                self._reset_delay()  # reset the delay after a successful connection
-                self._save_tweets(r)  # save tweets to disk
-                r.close()
-                return
-        
+            try: 
+                for uid in self.user_ids:
+                    self.process_user(uid)
+
             except KeyboardInterrupt:
                 try:
                     self._close_files()
@@ -71,56 +85,79 @@ class Stream(object):
                     pass
                 print
                 return
-            
-            except IOError, e:
-                print "%s: %s" % (type(e).__name__, e)
-                return
-
-            except Exception, e:
-                # catch dropped stream and try to reconnect:
-                # - TwitterAPI throws TwitterRequestError, TwitterConnectionError
-                # - httplib.IncompleteRead triggers the latter
+        
+           except Exception, e:
+                # Most likely a TwitterRequestError - continue with next user, but with 
+                # increasing delays in case it's a connection or rate limit issue
                 self.delay = self.delay * 2
-                print "\n" + str(dt.now())
-                print "%s: %s" % (type(e).__name__, e)
-                print "Attempting to reconnect after %d sec. delay" % (self.delay,)
+                print("\n" + str(dt.now()))
+                print("{}: {}".format(type(e).__name__, e))
+                print("Attempting to reconnect after {} sec. delay".format(self.delay))
                 time.sleep(self.delay)
                 continue
-    
-    
-    def _save_tweets(self, r):
-        '''Read tweets from the open connection and save to disk'''
 
-        for tweet in r.get_iterator():
-
-            # log stall warnings
-            if 'warning' in tweet:
-                print "\n" + str(dt.now())
-                print tweet['warning']
-                continue
-
-            # initialize new output file if tweet count is 0
-            if (self.tcount == 0):
-                ts = dt.now().strftime('%Y%m%d-%H%M%S')
-                self.fpath = OUTPUT_PATH + self.fname_base + ts + '.json'
-                self.f = open(self.fpath, 'w')
+        
+    def process_user(self, uid):
+        '''Download posts for one user'''
     
-            # save to file, or skip if the data is incomplete or has encoding errors
-            try:
-                self.f.write(json.dumps(tweet) + '\n')
-                self.tcount += 1
-            except:
-                continue
-    
-            # close the output file when it fills up
-            if (self.tcount >= ROWS_PER_FILE):
-                self._close_files()
-                self.tcount = 0
+        last_post_id = None
+        last_ts = None
+        try:
+            r = page_tweets(uid, last_post_id)
+            
+            response_len = 0
+            for item in r.get_iterator():
+                response_len += 1
+                try:
+                    last_id = item['id']
+                    last_ts = time.strptime(item['created_at'], 
+                            '%a %b %d %H:%M:%S +0000 %Y')
+                    if (last_ts >= self.ts_min) & (last_ts < self.ts_max):
+                        if (not self.geo_only):
+                            self._save_item(item)
+                        elif (self.geo_only & (item['coordinates'] | item['place'])):
+                            self._save_item(item)
+                        
+                except ValueError:
+                    # A necessary key is missing, either because it's a status  message 
+                    # or the tweet is badly formed
+                    if 'message' in item:
+                        self._save_item(item)
+                    else:
+                        continue
+            
+            time.sleep(RATE_LIMIT)
 
-            # if we reach a time limit, end the script
-            if (self.time_limit > 0) & ((time.time() - self.t0) >= self.time_limit):
-                self._close_files()
+            if (response_len == 0):
+                # No more tweets left from a user
                 return
+                
+            if (last_ts < self.ts_min):
+                # We've passed the time window of interest
+                # Warning: will break too soon if there are native retweets
+                return
+        
+    
+    def _save_item(self, item):
+        '''Save item to disk'''
+
+        # initialize new output file if tweet count is 0
+        if (self.tcount == 0):
+            ts = dt.now().strftime('%Y%m%d-%H%M%S')
+            self.fpath = OUTPUT_PATH + self.fname_base + ts + '.json'
+            self.f = open(self.fpath, 'w')
+
+        # save to file, or skip if the data is incomplete or has encoding errors
+        try:
+            self.f.write(json.dumps(item) + '\n')
+            self.tcount += 1
+        except:
+            continue
+
+        # close the output file when it fills up
+        if (self.tcount >= ROWS_PER_FILE):
+            self._close_files()
+            self.tcount = 0
 
 
     def _close_files(self):
@@ -139,4 +176,3 @@ class Stream(object):
             # delete the uncompressed copy
             os.remove(self.fpath)     
     
-
