52320b5a5da9c063075b70b7be34aa8ba6ac1a24
Sam Maurer <maurer@berkeley.edu>
2017-09-19 22:32:45 -0700

    Revising to sample 1000

--- test.py
+++ test.py
@@ -16,8 +16,8 @@ users = pd.read_csv('data/user_sample.csv').user_id.tolist()
 
 a = rest_automator.Automator(
         user_ids = users,
-        ts_min = time.strptime('Jan 1 2016 0:00:00', '%b %d %Y %H:%M:%S'),
-        ts_max = time.strptime('Jan 1 2017 0:00:00', '%b %d %Y %H:%M:%S'),
+        ts_min = time.strptime('Oct 1 2015 0:00:00', '%b %d %Y %H:%M:%S'),
+        ts_max = time.strptime('Oct 1 2016 0:00:00', '%b %d %Y %H:%M:%S'),
         geo_only = False,
 		fname_base = 'sample-')
 
